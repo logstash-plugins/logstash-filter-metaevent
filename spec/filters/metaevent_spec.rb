63c94b2e0beff98873bddd7f997a158d8df6e520
Pier-Hugues Pellerin <phpellerin@gmail.com>
2014-11-14 14:32:58 -0500

    Fix the gemspec, add License, test the register method
    missing the filter group

--- spec/filters/metaevent_spec.rb
+++ spec/filters/metaevent_spec.rb
@@ -2,5 +2,7 @@ require 'spec_helper'
 require 'logstash/filters/metaevent'
 
 describe LogStash::Filters::Metaevent do
-
+  it 'tries to register the plugin' do
+    expect(LogStash::Filters::Metaevent.new({ "followed_by_tags" => "abc" }).register).to eq([])
+  end
 end
