26c040b629fe500870538ced5844c3960fca67eb
Tal Levy <jubboy333@gmail.com>
2016-09-09 15:37:29 -0700

    update to new plugin api

--- lib/logstash/filters/metaevent.rb
+++ lib/logstash/filters/metaevent.rb
@@ -50,19 +50,19 @@ class LogStash::Filters::Metaevent < LogStash::Filters::Base
     @logger.debug(["metaevent", @add_tag, "trigger", event])
 
     event = LogStash::Event.new
-    event["source"] = Socket.gethostname
-    event["tags"] = [@add_tag]
+    event.set("source", Socket.gethostname)
+    event.set("tags", [@add_tag])
 
     @metaevents << event
     @start_event = nil
   end
 
   def followed_by_tags_match(event)
-    (event["tags"] & @followed_by_tags).size == @followed_by_tags.size
+    (event.get("tags") & @followed_by_tags).size == @followed_by_tags.size
   end
 
   def within_period(event)
-    time_delta = event["@timestamp"] - @start_event["@timestamp"]
+    time_delta = event.get("@timestamp") - @start_event["@timestamp"]
     time_delta >= 0 && time_delta <= @period
   end
 end
